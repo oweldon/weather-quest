b9d11c33b7bc1ed95cc82405163431ab4818efa9
Orion Weldon <weldon.orion@gmail.com>
2018-04-21 00:48:26 -0700

    adding css

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Titles = () => (
   <div>
-    <h1>WEATHER QUEST</h1>
-    <p>A Warriors Guide To Weather. To Better Win The Day</p>
+    <h1 className="title-container__title">WEATHER QUEST</h1>
+    <h3 className="title-container__subtitle">A Warriors Guide To Weather. To Better Win The Day</h3>
   </div>
 );
 
