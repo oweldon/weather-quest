e8a5f6ea0093721b7aed062b9148740f1ab69d35
Orion Weldon <weldon.orion@gmail.com>
2018-04-20 21:16:58 -0700

    succesfully retrieve json from weather api

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather"
 
+const weather_key = process.env.REACT_APP_WEATHER_API_KEY;
+
 class App extends React.Component {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Seattle,usa&appid=${weather_key}&units=imperial`);
+    const data = await api_call.json();
+    console.log(data);
+  }
+
   render() {
     return(
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather}/>
         <Weather />
       </div>
     );
