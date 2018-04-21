ecc3c8f5667ddcd2e720110198846b6bdb488df1
Orion Weldon <weldon.orion@gmail.com>
2018-04-20 20:08:36 -0700

    stubbed out components

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Titles extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>WEATHER QUEST</h1>
+        <p>A Warriors Guide To Weather. To Better Win The Day</p>
+      </div>
+    );
+  }
+}
+
+export default Titles;
