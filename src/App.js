5ea88ccc1b93605d6a6e6b57c3bfee8d145ef338
Orion Weldon <weldon.orion@gmail.com>
2018-04-20 21:26:20 -0700

    retreive json based on form input

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ const weather_key = process.env.REACT_APP_WEATHER_API_KEY;
 class App extends React.Component {
   getWeather = async (e) => {
     e.preventDefault();
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Seattle,usa&appid=${weather_key}&units=imperial`);
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${weather_key}&units=imperial`);
     const data = await api_call.json();
     console.log(data);
   }
