97b7de6c394402e491628ea3bb1e3f3bf88322b7
Orion Weldon <weldon.orion@gmail.com>
2018-04-21 18:36:20 -0700

    changed subtitle

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -3,7 +3,7 @@ import React from "react";
 const Titles = () => (
   <div className="title-container__all">
     <h1 className="title-container__title">WEATHER QUEST</h1>
-    <h3 className="title-container__subtitle">A Warriors Guide To Weather. To Better Win The Day</h3>
+    <h3 className="title-container__subtitle">Plan better. Live Better</h3>
   </div>
 );
 
